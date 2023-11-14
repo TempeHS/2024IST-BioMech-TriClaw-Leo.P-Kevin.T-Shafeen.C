30139bfc142367114b52dbfbefdb7d933473d05e
kevintran120 <kevin.tran120@education.nsw.gov.au>
2023-11-14 11:28:14 +1100

    did something
    with kevint ino

--- KevinT/KevinT.ino
+++ KevinT/KevinT.ino
@@ -1,4 +1,4 @@
-﻿/*
+/*
   Purpose: Basic example of a 180deg Micro Servo connected via a Seead Screw Terminal
   Notes: 
     1. Connect to a Digital Pin
@@ -12,15 +12,15 @@
 
 Servo myServo;  // create servo object to control a servo
 
-static unsigned int degreeOfMovement = 180;    // variable for angle of servo
+static unsigned int degreeOfMovement = 180;    // variable to read the value from the analog pin
 
 void setup() {
-  myServo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myServo.attach(8);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
   myServo.write(degreeOfMovement);                // sets the servo position according to the set value
   delay(5000);                                    // waits for the servo to get there
-  myServo.write(0);                               // returns the servo to 0 degrees
+  myServo.write(0);                               // returns the servo to 0
   delay(5000);                                    // waits for the servo to get there
 }
